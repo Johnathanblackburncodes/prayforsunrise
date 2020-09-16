26ff8aa27af45bd0440eb62ac464cea067b6f1a3
Sweet Van Loan <Sweet@SweetVanLoan.com>
2020-09-16 17:55:47 -0500

    signup changes, need auth

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+import uuid
 
 # Create your views here.
 
