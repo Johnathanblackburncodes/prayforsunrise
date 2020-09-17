edc20aa40eed125de0fca64084a1d086a5ca5670
NathanChildress <r.nathanchildress@gmail.com>
2020-09-17 16:43:14 -0700

    game setsup for host and assigns cards

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add_game', views.add_game, name='add_game'),
+    path('setup_game', views.setup_game, name='setup_game'),
     path('accounts/signup/', views.signup, name='signup'),
     path('rooms/<str:room_name>/', views.room, name='room'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,11 +3,12 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-import uuid
+import uuid, random
 from django.contrib.auth.models import User
-from .forms import GameForm
-from .models import Game, Hand
+from .forms import GameForm, SetupGameForm
+from .models import Game, Card, Hand, STAGES
 
+V_STAGES = [t[0] for t in STAGES if t[0]]
 # Create your views here.
 
 
@@ -15,15 +16,16 @@ def home(request):
     return render(request, 'home.html')
 
 def room(request, room_name):
-    # try:
-    game = Game.objects.get(room=room_name)
-    room_user = User.objects.get(pk=request.user.id)
-    game.user.add(room_user)
-    # except:
-    #     print(game)
-    #     return redirect('/')
+    try:
+        game = Game.objects.get(room=room_name)
+        room_user = User.objects.get(pk=request.user.id)
+        game.user.add(room_user)
+    except:
+        print(game)
+        return redirect('/')
     return render(request, 'game/room.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'game': game
     })
 
 
@@ -47,6 +49,38 @@ def add_game(request ):
       print(f'{form}')
   return redirect(new_room)
 
+def setup_game( request ):
+    form = SetupGameForm(request.POST)
+    if form.is_valid():
+        update_game=form.save(commit=False)
+    #None of the above is doing anything but it's a placeholder while we brute force setup
+    start_game = Game.objects.get(room=update_game.room)
+    
+    index_of_stage = V_STAGES.index(start_game.stage)
+    #gets the list of users in our game
+    players = start_game.user.all()
+    
+    #lets create a hand for each user in the game.
+    for player in players:
+        print(f'{player} in {players}')
+        new_hand = Hand()
+        new_hand.game = start_game
+        new_hand.user = player
+        rand_card = random.choice(Card.objects.all())
+        new_hand.card = rand_card
+        new_hand.save()
+    start_game.stage = STAGES[index_of_stage+1][0]
+    start_game.save()
+    return redirect('rooms/' + update_game.room)
+
+
+
+
+    
+    
+        
+
+
 def signup(request):
     error_message = ''
     if request.method == 'POST':
@@ -65,3 +99,4 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
