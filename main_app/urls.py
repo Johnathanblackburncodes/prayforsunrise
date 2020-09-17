bf5c60c6e53cebd405965d572491f5ba34bfadcc
NathanChildress <r.nathanchildress@gmail.com>
2020-09-17 14:21:56 -0700

    addsusers to game as they join chat.

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('rooms/<str:room_name>/', views.room, name='room'),
+    path('add_game', views.add_game, name='add_game'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('rooms/<str:room_name>/', views.room, name='room'),
 ]
