de5960df922672af1c8d59f2a0c603e259468aff
Sweet Van Loan <Sweet@SweetVanLoan.com>
2020-09-18 15:56:39 -0500

    with updates

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,7 +13,7 @@ from . import views
 #testing USER case
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add_game', views.add_game, name='add_game'),
+    path('add_game/', views.add_game, name='add_game'),
     path('setup_game', views.setup_game, name='setup_game'),
     path('accounts/signup/', views.signup, name='signup'),
     path('profile/<int:user_id>/add_photo/', views.add_photo, name='add_photo'), #added for AWS profiles, referencing profile class SVL
